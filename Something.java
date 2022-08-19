b2d2c12a523ca269d96149ba19aa2deeffd442a8
hashim715 <mharus575@gmail.com>
2022-08-19 09:44:21 +0500

    Developed something

--- Something.java
+++ Something.java
@@ -1,6 +1,7 @@
 class Something {
  public static void main(String args[]){
     System.out.println("hello world");
+    System.out.println("under developement");
   //we are still developing wait you busted
 }
 }
